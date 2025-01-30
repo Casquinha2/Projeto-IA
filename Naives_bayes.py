3bb40e7473b97aecc3ca070e31dc08e04490bba9
Casquinha2 <121224053+Casquinha2@users.noreply.github.com>
2025-01-30 17:03:32 +0000

    Update Naives_bayes.py

--- Naives_bayes.py
+++ Naives_bayes.py
@@ -13,50 +13,41 @@ token_client = 'AQUI'
 #Inserir o nome do/dos canal/canais da twitch
 canal = ['AQUI']
 
-
-
-
+#Leitura da base de dados
 df = pd.read_csv('ham_spam_xqcow_and_sodapoppin.csv', sep='\t', header=None, names=['label', 'message'])
-
 df[['label', 'message']] = df['label'].str.split(n=1, expand=True)
-
 df['message'] = df['message'].astype(str).str.strip('"')
-
-
-
 df['label'].value_counts()
-
-
 df['label_encoded'] = df['label'].map({'ham': 0, 'spam': 1})
 
-
+#Definir as variáveis
 X = df['message']
 y = df['label_encoded']
+
+#Divisão da base de dados
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-
+#Vetorização das variáveis X
 vectorizer = CountVectorizer()
 X_train_counts = vectorizer.fit_transform(X_train)
 X_test_counts = vectorizer.transform(X_test)
 
 
-# Inicializar o classificador Naive Bayes Multinomial
+#Inicializar o classificador Naive Bayes Multinomial
 clf = MultinomialNB()
 
-# Treinar o modelo com os dados de treinamento
+#Treinar o modelo com os dados de treinamento
 clf.fit(X_train_counts, y_train)
 
-
+#Previsão dos Y
 y_pred = clf.predict(X_test_counts)
 
-
+#Precisão do método
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Precisão: {accuracy:.4f}")
 
-
+#Mostrar tabela com estatísticas
 print(classification_report(y_test, y_pred, target_names=['Ham', 'Spam']))
-
-
 cm = confusion_matrix(y_test, y_pred)
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=['Ham', 'Spam'], yticklabels=['Ham', 'Spam'])
 plt.ylabel('Real')
@@ -64,7 +55,7 @@ plt.xlabel('Previsto')
 plt.title('Matriz de Confusão')
 plt.show()
 
-
+#Comandos para o Bot da Twitch
 class Bot(commands.Bot):
 
     def __init__(self):
@@ -99,5 +90,14 @@ class Bot(commands.Bot):
 
         await self.handle_commands(message)
 
-bot = Bot()
-bot.run()
+#Tenta correr o código para o Bot da Twitch
+try:
+    bot = Bot()
+    bot.run()
+except Exception as e:
+    print(f"""\nO programa não dá para usar com a conta da twitch.
+Erros possíveis:
+    - Não querer fazer a interligação com a Twitch
+    - Token mal inserido
+    - Canal inexistente
+Erro: {e}""")
